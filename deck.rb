663ebdd4bf728439d9ac462e720aa6b836b87f1c
Havenwood <shannonskipper@gmail.com>
2011-11-02 20:50:26 -0700

    slight refactor

--- deck.rb
+++ deck.rb
@@ -28,18 +28,17 @@ class Array
   end
 
   def value
-    rank = self.first.split.first
-    case rank
-    when "Jack"
-      11
-    when "Queen"
-      12
-    when "King"
-      13
+    case self.first.split.first
     when "Ace"
       14
+    when "King"
+      13
+    when "Queen"
+      12
+    when "Jack"
+      11
     else
-      rank.to_i
+      rank
     end
   end
 end
