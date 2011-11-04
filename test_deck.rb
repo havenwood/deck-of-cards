e82c31f64f8e9b71386471034c549f4ec5b92998
Havenwood <shannonskipper@gmail.com>
2011-11-03 17:00:42 -0700

    switched from unit to spec style

--- test_deck.rb
+++ test_deck.rb
@@ -1,25 +1,18 @@
 #!/usr/bin/env ruby
 require 'rubygems'
-gem 'minitest' # use minitest gem, not 1.9.x built-in version
+gem 'minitest'
 require 'minitest/autorun'
-require 'minitest/pride' # pretty colors
-require './deck' # class being tested
+require 'minitest/pride'
+require './deck'
 
-class TestDeck < MiniTest::Unit::TestCase
-  def setup
+describe Deck do
+  before do
     @deck = Deck.new
   end
   
-  def test_that_a_new_deck_can_be_made
-    assert_instance_of Deck, @deck
-  end
-  
-  def test_that_a_deck_can_be_shuffled
-    assert_kind_of Array, @deck.shuffle
-  end
-  
-  def test_that_cards_can_be_drawn_from_deck
-    assert_kind_of Array, @deck.draw
-    assert_equal @deck.draw(3).size, 3 
+  describe "when a deck is created" do
+    it "must exist" do
+      @deck.must_be_instance_of Deck
+    end
   end
 end
