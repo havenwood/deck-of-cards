406de3dba3bc1db0e7a2a042363b1aa774f85cce
Shannon Skipper <shannonskipper@gmail.com>
2012-04-01 18:07:30 -0700

    added tests for existing methods

--- test_deck.rb
+++ test_deck.rb
@@ -7,17 +7,64 @@ rescue LoadError
   require 'minitest/autorun'
 ensure
   require 'minitest/pride'
+  require 'set'
+  require 'pry'
   require './deck'
 end
 
 describe Deck do
   before do
     @deck = Deck.new
+    
+    @unshuffled_cards = @deck
+    
+    @correct_cards = []
+    suits = ["Hearts", "Spades", "Diamonds", "Clubs"]
+    ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+    suits.each { |suit| ranks.each { |rank| @correct_cards << "#{rank} of #{suit}" } }
   end
   
   describe "when a deck is created" do
-    it "must exist" do
+    it "must be a deck of cards" do
       @deck.must_be_instance_of Deck
+    end
+    
+    it "must have 52 cards" do
+      @deck.cards.count.must_equal 52
+    end
+    
+    it "must have the correct cards" do
+      @deck.cards.must_equal @correct_cards
+    end
+  end
+  
+  describe "when a deck is shuffled" do
+    it "wont be in the same order" do
+      @deck.shuffle
+      @deck.cards.wont_equal @unshuffled_cards
+    end
+  end
+  
+  describe "when a deck is cut" do
+    it "must have 52 cards" do
+      @deck.cut
+      @deck.cards.count.must_equal 52
+    end
+    
+    it "must have the correct cards" do
+      @deck.cut
+      @deck.cards.to_set.must_equal @correct_cards.to_set
+    end
+    
+    it "wont be in the same order" do
+      @deck.cut
+      @deck.cards.wont_equal @unshuffled_cards
+    end
+  end
+  
+  describe "when a card is compared" do
+    it "will win!" do
+      true
     end
   end
 end
